f46bb3e56198bd2042f5ad25bd96aa278af3bbcc
admin <pal.prashant31@gmail.com>
2022-03-01 15:21:32 +0530

    Fixed parallel

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 )
 
 func main() {
-	file, err := ioutil.ReadFile("fixtures/stage_with_10_test.yaml")
-	if err != nil {
-		log.Fatalf("Error Reading fixture %v", err.Error())
-	}
-
 	pipeline := NewPipeline(
-		"Parallel150Stages",
-		"stages150Parallel",
 		"CIProject",
 		"default",
 		true,
-		string(file),
-		300)
-	err = pipeline.GenerateYaml()
+		200)
+	err := pipeline.GenerateYaml()
 	if err != nil {
 		log.Fatalf("%v", err.Error())
 	}
--- pipeline.go
+++ pipeline.go
@@ -8,28 +8,31 @@ import (
 )
 
 type Pipeline struct {
-	Name         string
-	Identifier   string
-	Project      string
-	Org          string
-	IsParallel   bool
-	StageSnippet string
-	Sl           []int
+	Name       string
+	Identifier string
+	Project    string
+	Org        string
+	IsParallel bool
+	Sl         []int
 }
 
-func NewPipeline(name string, identifier string, project string, org string, isParallel bool, stageSnippet string, stageCount int) *Pipeline {
+func NewPipeline(project string, org string, isParallel bool, stageCount int) *Pipeline {
 	sl := make([]int, stageCount)
 	for i := 0; i < stageCount; i++ {
 		sl[i] = i
 	}
+	suffix := ""
+	if isParallel {
+		suffix = "parallel"
+	}
+	name := fmt.Sprintf("Stages%d%s", stageCount, suffix)
 	return &Pipeline{
-		Name:         name,
-		Identifier:   identifier,
-		Project:      project,
-		Org:          org,
-		IsParallel:   isParallel,
-		StageSnippet: stageSnippet,
-		Sl:           sl,
+		Name:       name,
+		Identifier: name,
+		Project:    project,
+		Org:        org,
+		IsParallel: isParallel,
+		Sl:         sl,
 	}
 }
 
